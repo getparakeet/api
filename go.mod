4eabe9d2c327f80ab148985c8c7bbf6d20dfd094
lerichardson <litbelb1@outlook.com>
2021-08-31 13:49:07 -0700

    DB connection

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/getparakeet/api
 
 go 1.16
 
-require github.com/gofiber/fiber v1.14.6
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.10.2
+)
--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+
 	"github.com/gofiber/fiber"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+func db() {
+	psqlconn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", os.Getenv("PSQL_USER"), os.Getenv("PSQL_PWD"), os.Getenv("PSQL_HOST"), os.Getenv("PSQL_PORT"), os.Getenv("PSQL_DB"))
+	fmt.Println(psqlconn)
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected!")
+}
 func main() {
+	godotenv.Load()
+	db()
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) {
+	app.Get("/v1/verify/key", func(c *fiber.Ctx) {
 		c.Send("Hello, World!")
 	})
 }
