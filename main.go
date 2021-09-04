ee7c23e6b6dbd2e5a12f07a1c70337ce147d55ec
lerichardson <litbelb1@outlook.com>
2021-09-03 20:00:30 -0700

    add keychecking

--- main.go
+++ main.go
@@ -4,31 +4,54 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gofiber/fiber"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-func db() {
-	psqlconn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", os.Getenv("PSQL_USER"), os.Getenv("PSQL_PWD"), os.Getenv("PSQL_HOST"), os.Getenv("PSQL_PORT"), os.Getenv("PSQL_DB"))
-	db, err := sql.Open("postgres", psqlconn)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Connected!")
+type KeyData struct {
+	Title string `json:"projectTitle"`
+	Key   string `json:"key"`
 }
+
 func main() {
 	godotenv.Load()
-	db()
 	app := fiber.New()
-	app.Get("/v1/verify/key", func(c *fiber.Ctx) {
-		c.Send("Hello, World!")
+	app.Get("/v1/verify/key", func(res *fiber.Ctx) {
+		fields := new(KeyData)
+		if err := res.BodyParser(fields); err != nil {
+			panic(err)
+		}
+		query := `SELECT "name", "key" FROM "keydata"`
+		psqlconn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", os.Getenv("PSQL_USER"), os.Getenv("PSQL_PWD"), os.Getenv("PSQL_HOST"), os.Getenv("PSQL_PORT"), os.Getenv("PSQL_DB"))
+		db, err := sql.Open("postgres", psqlconn)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+		resp, err := db.Query(query)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if strings.Contains(query, "SELECT") {
+			for resp.Next() {
+				var name string
+				var key string
+
+				err := resp.Scan(&name, &key)
+				if err != nil {
+					panic(err)
+				}
+				if fields.Key != key {
+					res.Status(400).Send("Invalid key")
+				} else if fields.Key == key {
+					res.Status(200).Send("Valid key")
+				}
+			}
+		}
+		defer resp.Close()
 	})
+	app.Listen(":3000")
 }
